e51e2de53711dbd4ba06d8f5b04389ac0614f692
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-08-20 08:25:02 -0300

    Add files via upload

---
+++ Coletor/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BarcodeScannerApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
